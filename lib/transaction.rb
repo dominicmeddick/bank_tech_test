b9b5f318d173b24fcf7379df02c72950533044b2
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-02 10:42:21 +0100

    passing test for initialize method with instance variable record

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction
 
+  attr_reader :record
+
+  def initialize
+    @record = [:date, :credit, :debit, :balance]
+  end
+
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,5 +1,9 @@
 require 'transaction'
 
 describe Transaction do
+
+  it 'should record the four given criteria' do
+    expect(subject.record).to eq [:date, :credit, :debit, :balance]
+  end
   
 end
