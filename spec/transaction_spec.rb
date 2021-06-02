993d69833633f5bb685e7ad148295c54c90af0f8
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-02 11:04:38 +0100

    Test added to test values for record hash

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,8 +4,12 @@ describe Transaction do
 
   let(:transaction) { Transaction.new}
 
-  it 'should record the four given criteria' do
-    expect(transaction.record.keys).to eq [:date, :credit, :debit, :balance]
+  it 'should record the four keys of the given criteria' do
+    expect(transaction.record.keys).to eq [ :date, :credit, :debit, :balance ]
+  end
+
+  it 'should record the four values of the given criteria' do
+    expect(transaction.record.values).to eq [ nil, nil, nil, nil ]
   end
   
 end
